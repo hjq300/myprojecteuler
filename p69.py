6900d97c6676a22fc006486e7657e4fa80858e2e
hjq-debian <hjq_hjq2008@163.com>
2014-11-30 23:35:54 +0800

    modified:   p69.py         new file:   tools/mytools.py

--- p69.py
+++ p69.py
@@ -1,8 +1,14 @@
-import sympy
-# gen prime table
-T = {}
-N = 1000000
-for i in range(2,N+1):
-    if sympy.isprime(i):
-        T[i] = True
+#!/usr/bin/env python
 
+N = 100
+t = {}
+for i in range(2,N+1):
+    t[i] = i
+for i in range(1,N+1):
+    print i
+    for n in range(2,N+1):
+        m = i*n
+        if m > N:
+            break
+        else:
+            t[m] -= 1
